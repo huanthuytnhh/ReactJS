72e9362d16949e25ea7e12eba914d1f394b685a2
huanthuytnhh <huanthuytnhh@gmail.com>
2024-09-08 20:59:58 +0700

    first commit

---
+++ 0.EnhancedObjectLiteral/app.js
@@ -0,0 +1,3 @@
+//1 Dinh nghia key=value cho object vd var name = "Thanh";var price=100;
+//2 Dinh nghia method cho object (lược bỏ function)
+//3 lấy value của 1 biến làm key fieldName = "name";
---
+++ 0.Module/app.js
@@ -0,0 +1,5 @@
+import logger from "./logger.js";
+// import { TYPE_LOG, TYPE_WARN, TYPE_ERROR } from "./constants.js";
+// C2
+import * as constants from "./constants.js";
+logger("Testing.....", constants.TYPE_WARN);
